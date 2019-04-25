8d7621076858b23483167f8b31d1c41656516970
Marzena <45318218+marziflora@users.noreply.github.com>
2019-04-25 18:17:39 +0200

    Update exon.py

--- exon.py
+++ exon.py
@@ -3,12 +3,12 @@ import time
 
 t0 = time.time()
 
-path = "C:/Users/HP/Desktop/Studia/MGR/GFF3/Bos_taurus.UMD3.1.94.chr.gff3"  #plik ze wszystkimi chromosomami
+path = " "  #plik ze wszystkimi chromosomami
 with open(path, "r") as f:
-    for line in f:  #wczytuj po linijce
+    for line in f: 
         if line[0].isdigit() or line[0] == 'X' or line[0:2] == 'MT':  #jeżeli 1 znak jest cyfrą (chromosom) / X / MT
             p = bool(re.search("exon", line))  #logiczna zmienna: Czy jest exon w linijce?
-            if p == True:  #jeżeli znajdzie:
+            if p == True:  
                 r = open("Bos_taurus.UMD3.1.94.chr.gff3-exon.txt", "a") #Zapisz w nowym pliku
                 l = [num for num in line.split('\t')]  #dzieli linijkę na wyrazy przedzielone tabulatorem
                 string = " ".join(l[0:8])  #wybór pierwszych 8 słów
